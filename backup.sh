457cecd70694b7b30a0a6f593865452b7688c034
JohannesNakayama <johannes.nakayama@rwth-aachen.de>
2022-12-30 10:43:14 +0100

    Create backup script

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BACKUP_DATETIME=$(date +"%Y-%m-%d--%H-%M-%S")
+BACKUP_HOSTNAME=$(hostname)
+BACKUP_FILENAME="${BACKUP_DATETIME}-backup-${BACKUP_HOSTNAME}.tar.gz"
+
+cd
+
+tar -czvf $BACKUP_FILENAME \
+    ./Desktop \
+    ./Documents \
+    ./Downloads \
+    ./Music \
+    ./Pictures \
+    ./Projects \
+    ./Public \
+    ./Videos
+
