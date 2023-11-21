d39d36a8a5cde09c5b963ae5d0c3b15fadc96d53
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2023-11-21 14:15:29 +0100

    Update backup script

--- backup.sh
+++ backup.sh
@@ -2,17 +2,13 @@
 
 BACKUP_DATETIME=$(date +"%Y-%m-%d--%H-%M-%S")
 BACKUP_HOSTNAME=$(hostname)
-BACKUP_FILENAME="~/${BACKUP_DATETIME}-backup-${BACKUP_HOSTNAME}.tar.gz"
+BACKUP_FILENAME="/home/johannes/${BACKUP_DATETIME}-backup-${BACKUP_HOSTNAME}.tar.gz"
 
 cd ~
 
 tar -czvf $BACKUP_FILENAME \
     ./Desktop \
     ./Documents \
-    ./Downloads \
-    ./Music \
     ./Pictures \
-    ./Projects \
-    ./Public \
-    ./Videos
+    ./Projects
 
