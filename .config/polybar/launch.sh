9b3e9adfdb38a91846a784fbcff1c9b8e57e7a0f
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2023-11-26 15:38:28 +0100

    Reconfigure polybar
    - persistent issue: launch script doesn't kill previous instances

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Kill all running polybars
-killall -q polybar
+# killall -q polybar &
+# kill -9 $(pgrep -f 'polybar') > /dev/null 2>&1
+# kill $(ps aux | grep 'polybar' | awk '{print $2}')  >/dev/null 2>&1
+polybar-msg cmd quit
 
 # Wait until killing is done
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch main bar
-# -> adapted from https://protesilaos.com/codelog/multihead-bspwm-polybar/
-polybar main &
+polybar -r main &
 
-# Launch bar for external monitor
-# --> adapted from https://protesilaos.com/codelog/multihead-bspwm-polybar/
+# # Launch bar for external monitor
+# # --> adapted from https://protesilaos.com/codelog/multihead-bspwm-polybar/
 external_monitor=$(xrandr --query | grep 'HDMI-1')
 if [[ $external_monitor = *connected* ]]; then
-    polybar main_external &
+  polybar -r main_external &
 fi
