1338f723a5874ad38116b24117fb9f473d80c75c
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-06-22 13:32:44 +0200

    Iterate polybar
    - nicer layout
    - far better font and icons
    - things work now!

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-# Kill all running polybars
-# killall -q polybar &
-# kill -9 $(pgrep -f 'polybar') > /dev/null 2>&1
-# kill $(ps aux | grep 'polybar' | awk '{print $2}')  >/dev/null 2>&1
+# Terminate all running polybars
 polybar-msg cmd quit
 
-# Wait until killing is done
-# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
 # Launch main bar
-polybar -r main-boring &
+polybar -r main &
 
 # # Launch bar for external monitor
 # # --> adapted from https://protesilaos.com/codelog/multihead-bspwm-polybar/
