64d10665587bf1fe048d46931620459e8ebb583d
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2025-08-22 17:28:48 +0200

    Comment out external monitor setting for polybar
    - enable explicitly

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ polybar -r main &
 
 # # Launch bar for external monitor
 # # --> adapted from https://protesilaos.com/codelog/multihead-bspwm-polybar/
-external_monitor=$(xrandr --query | grep 'HDMI-1')
-if [[ $external_monitor = *connected* ]]; then
-  polybar -r main_external &
-fi
+# external_monitor=$(xrandr --query | grep 'HDMI-1')
+# if [[ $external_monitor = *connected* ]]; then
+#   polybar -r main_external &
+# fi
