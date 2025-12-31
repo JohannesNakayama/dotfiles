737f35cc105c43f8362d06a93eda8a8a427c5e15
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2025-12-31 09:58:25 +0100

    fix: remove common-pc-laptop hardware optimizations
    - battery optimizations too aggressive

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -36,7 +36,6 @@
           # Apply hardware optimizations.
           nixos-hardware.nixosModules.common-cpu-amd
           nixos-hardware.nixosModules.common-gpu-amd
-          nixos-hardware.nixosModules.common-pc-laptop
           nixos-hardware.nixosModules.common-pc-ssd
 
           # Load home manager module.
