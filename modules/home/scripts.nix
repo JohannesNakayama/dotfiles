62ecc1819d98e90131e692cb81fdf33147746c0f
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2026-01-02 23:43:55 +0100

    fix: script paths in scripts.nix module

--- modules/home/scripts.nix
+++ modules/home/scripts.nix
@@ -10,18 +10,18 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = [
-      (pkgs.writeShellScriptBin "backup" (builtins.readFile ../scripts/backup))
-      (pkgs.writeShellScriptBin "byebye" (builtins.readFile ../scripts/byebye))
-      (pkgs.writeShellScriptBin "cfg" (builtins.readFile ../scripts/cfg))
-      (pkgs.writeShellScriptBin "note" (builtins.readFile ../scripts/note))
-      (pkgs.writeShellScriptBin "post" (builtins.readFile ../scripts/post))
-      (pkgs.writeShellScriptBin "recipe" (builtins.readFile ../scripts/recipe))
-      (pkgs.writeShellScriptBin "shop" (builtins.readFile ../scripts/shop))
-      (pkgs.writeShellScriptBin "tb" (builtins.readFile ../scripts/tb))
-      (pkgs.writeShellScriptBin "tigd" (builtins.readFile ../scripts/tigd))
-      (pkgs.writeShellScriptBin "todo" (builtins.readFile ../scripts/todo))
-      (pkgs.writeShellScriptBin "v" (builtins.readFile ../scripts/v))
-      (pkgs.writeShellScriptBin "wallpaper" (builtins.readFile ../scripts/wallpaper))
+      (pkgs.writeShellScriptBin "backup" (builtins.readFile ../../scripts/backup))
+      (pkgs.writeShellScriptBin "byebye" (builtins.readFile ../../scripts/byebye))
+      (pkgs.writeShellScriptBin "cfg" (builtins.readFile ../../scripts/cfg))
+      (pkgs.writeShellScriptBin "note" (builtins.readFile ../../scripts/note))
+      (pkgs.writeShellScriptBin "post" (builtins.readFile ../../scripts/post))
+      (pkgs.writeShellScriptBin "recipe" (builtins.readFile ../../scripts/recipe))
+      (pkgs.writeShellScriptBin "shop" (builtins.readFile ../../scripts/shop))
+      (pkgs.writeShellScriptBin "tb" (builtins.readFile ../../scripts/tb))
+      (pkgs.writeShellScriptBin "tigd" (builtins.readFile ../../scripts/tigd))
+      (pkgs.writeShellScriptBin "todo" (builtins.readFile ../../scripts/todo))
+      (pkgs.writeShellScriptBin "v" (builtins.readFile ../../scripts/v))
+      (pkgs.writeShellScriptBin "wallpaper" (builtins.readFile ../../scripts/wallpaper))
     ];
   };
 }
